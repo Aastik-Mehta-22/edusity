2b3a4370b784af61ad79afe60c6d7779233cd4f1
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-09-07 02:13:30 +0530

    frontend part done landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Title from './components/Title/Title'
 import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testinomials from './components/Testinomials/Testinomials'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
@@ -20,6 +21,8 @@ const App = () => {
       <Campus/>
       <Title subTitle='TESTIMONIALS' title='What student Says'/>
       <Testinomials/>
+      <Title subTitle='Contact Us' title='Get in touch'/>
+      <Contact />
       </div>
     </div>
   )
